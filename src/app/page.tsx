00014a8802cb7be5ada1c32cee5b8f95f468b90f
william-dantas-dev <shireo.gaming.dev@gmail.com>
2024-05-29 22:45:52 -0300

    without SSR

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,17 +1,14 @@
 "use client";
 import React from "react";
 import dynamic from 'next/dynamic';
-
-const Map = dynamic(() => import('@/components/map'), {
-  ssr: false,
+const MapWithNoSSR = dynamic(() => import('@/components/map'), {
+  ssr: false, // Desativa a renderização do lado do servidor para este componente
 });
-
-
 export default function Home() {
   return (
     <main className={"absolute top-0 left-0 right-0 bottom-0 flex flex-col"}>
       <h1  className={"z-50, text-white text-center my-4"}>Mapa 2D - Ashes Of Creation - Shiroe</h1>
-      <Map />
+      <MapWithNoSSR />
     </main>
   );
 }
--- src/components/map.tsx
+++ src/components/map.tsx
@@ -6,30 +6,22 @@ import 'leaflet/dist/leaflet.css';
 // Defina a URL do ícone personalizado
 const customIconUrl = 'https://cdn.jsdelivr.net/npm/leaflet@1.7.1/dist/images/marker-icon.png';
 
+const customIcon = new Icon({
+  iconUrl: customIconUrl,
+  iconSize: [25, 41],
+  iconAnchor: [12, 41],
+  popupAnchor: [1, -34],
+  tooltipAnchor: [16, -28],
+  shadowSize: [41, 41],
+});
 
+const bounds : LatLngBoundsExpression = [[51.56, -0.2], [51.53, -0.12]];
+const maxBounds : LatLngBoundsExpression =[[51.56, -0.2], [51.53, -0.12]];
 const minZoom = 15;
 const maxZoom = 20;
 const defaultZoom = 16;
 
 const Map: React.FC = () => {
-  const[customIcon, setCustomIcon] = useState<any>();
-  const[bounds, setBounds] = useState<any>();
-  const[maxBounds, setMaxBounds] = useState<any>();
-   useEffect(() => {
-    if (typeof window !== 'undefined') {
-      setCustomIcon(new Icon({
-        iconUrl: customIconUrl,
-        iconSize: [25, 41],
-        iconAnchor: [12, 41],
-        popupAnchor: [1, -34],
-        tooltipAnchor: [16, -28],
-        shadowSize: [41, 41],
-      }))
-      setBounds([[51.56, -0.2], [51.53, -0.12]]);
-      setMaxBounds([[51.56, -0.2], [51.53, -0.12]]);
-    }
-  }, []);
-
   const [markers, setMarkers] = useState<LatLngExpression[]>([]);
 
   const handleClick = (e: any) => {
@@ -41,14 +33,14 @@ const Map: React.FC = () => {
     <MapContainer 
       bounds={bounds} 
       maxBounds={maxBounds} 
-      style={{backgroundColor: 'black', overflow: 'hidden'}}
+      style={{backgroundColor: 'white', overflow: 'hidden'}}
       minZoom={minZoom}
       maxZoom={maxZoom}
       zoom={defaultZoom}
       attributionControl={false} // Remover o ícone de atribuição do Leaflet
       className='h-full w-full'
     >
-    <ImageOverlay url="/map.jpg" bounds={bounds as LatLngBoundsExpression} />
+    <ImageOverlay url="/map1.jpg" bounds={bounds as LatLngBoundsExpression} />
       {markers.map((position, index) => (
         <Marker key={index} position={position} icon={customIcon}>
           <Popup>Mark {index + 1}</Popup>
